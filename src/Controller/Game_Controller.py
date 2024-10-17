7f19c2fa7558dc8f203e7adf69ced41c17443b6f
Yutatsu-KMC <chenyuokc@gmail.com>
2024-10-18 07:56:58 +0800

    New Updates

---
+++ src/Controller/Game_Controller.py
@@ -0,0 +1,6 @@
+# Manages the overall flow of the game.
+# Coordinating the interaction between the Model and View.
+# It also uses the InputHandler to get user input.
+
+class GameController:
+    pass
