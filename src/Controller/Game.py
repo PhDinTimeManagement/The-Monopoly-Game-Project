3846dc4e56f0548228b29093e3a48775ab750560
PhDinTimeManagement <137915198+PhDinTimeManagement@users.noreply.github.com>
2024-10-18 10:42:57 +0800

    10.18_Morning

--- src/Controller/Game.py
+++
@@ -1,9 +0,0 @@
-#The class represents the game state, where the game is running on the computer
-class Game:
-    def __int__(self):
-        self.playerList = {} # a list to store all the current in game players. Stores Class User Objects
-
-
-    def storePlayers(self,user,playerID): #'user' is of type Class user, 'playerID of type int
-        self.playerList[user] = playerID  #ID must be unique
-        return True
