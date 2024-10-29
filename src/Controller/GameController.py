9e188f9d82e21d6bab048156088022405369da7f
PhDinTimeManagement <137915198+PhDinTimeManagement@users.noreply.github.com>
2024-10-29 17:05:26 +0800

    Touch Ups on Gameboard.py v5

--- src/Controller/GameController.py
+++ src/Controller/GameController.py
@@ -1,6 +1,31 @@
-# Manages the overall flow of the game.
-# Coordinating the interaction between the Model and View.
-# It also uses the InputHandler to get user input.
+# GameController.py
+from src.Model.Gameboard import *
+from src.Model.Player import *
+from src.Model.GameLogic import *
+from src.Model.InputHandler import *
 
 class GameController:
-    pass
+    def __init__(self):
+        self.board = Gameboard()
+        self.players = []
+        self.current_turn = 0
+        self.broke_players = []
+        self.input_handler = InputHandler()
+        self.game_round = 1
+
+    def start_game(self):
+        self.initialize_players()
+        while not GameLogic.game_ends(self.players):
+            self.play_round()
+            GameLogic.set_current_round(self.game_round + 1)
+
+    def initialize_players(self):
+        num_players = self.input_handler.get_name_or_players("Enter the number of players (2-6): ")
+        for i in range(num_players):
+            player_name = self.input_handler.get_name_or_players(f"Enter name for Player {i + 1}: ")
+            new_player = Player(player_name)
+            self.players.append(new_player)
+        print("Players initialized successfully.\n")
+
+    def end_game(self):
+        if GameLogic.game_ends():
